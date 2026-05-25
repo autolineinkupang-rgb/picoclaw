6b53dbeed570773af1b002e3d8e3b59d2ded1525
autolineinkupang-rgb <autolineinkupang@gmail.com>
2026-05-25 17:25:43 +0700

    Refactor config setup in entrypoint.sh
    Updated config handling to use envsubst for variable substitution.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,32 @@
+##!/bin/sh
+#set -e
+
+# Copy config from app to picoclaw home
+#mkdir -p "${HOME}/.picoclaw"
+#cp /app/config/config.json "${HOME}/.picoclaw/config.json"
+
+# Replace environment variables in config
+#sed -i "s|\\$TELEGRAM_BOT_TOKEN|${TELEGRAM_BOT_TOKEN}|g" "${HOME}/.picoclaw/config.json"
+#sed -i "s|\\$GROQ_API_KEY|${GROQ_API_KEY}|g" "${HOME}/.picoclaw/config.json"
+#sed -i "s|\\$TELEGRAM_USER_ID|${TELEGRAM_USER_ID}|g" "${HOME}/.picoclaw/config.json"
+
+# Remove stale PID file
+#rm -f "${HOME}/.picoclaw/.picoclaw.pid"
+
+#exec picoclaw gateway "$@"
+
 #!/bin/sh
 set -e
 
-# Copy config from app to picoclaw home
+# Copy config template from app to picoclaw home
 mkdir -p "${HOME}/.picoclaw"
-cp /app/config/config.json "${HOME}/.picoclaw/config.json"
 
-# Replace environment variables in config
-sed -i "s|\\$TELEGRAM_BOT_TOKEN|${TELEGRAM_BOT_TOKEN}|g" "${HOME}/.picoclaw/config.json"
-sed -i "s|\\$GROQ_API_KEY|${GROQ_API_KEY}|g" "${HOME}/.picoclaw/config.json"
-sed -i "s|\\$TELEGRAM_USER_ID|${TELEGRAM_USER_ID}|g" "${HOME}/.picoclaw/config.json"
+# Substitute environment variables into the config.
+# envsubst does plain variable substitution with NO regex interpretation,
+# so secrets containing :, /, &, \ etc. cannot break the substitution.
+envsubst '${TELEGRAM_BOT_TOKEN} ${GROQ_API_KEY} ${TELEGRAM_USER_ID}' \
+  < /app/config/config.json \
+  > "${HOME}/.picoclaw/config.json"
 
 # Remove stale PID file
 rm -f "${HOME}/.picoclaw/.picoclaw.pid"

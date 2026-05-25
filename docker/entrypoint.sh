47149b02cecc1cf4aee9310a419483e20947a817
autolineinkupang-rgb <autolineinkupang@gmail.com>
2026-05-25 14:44:50 +0700

    Simplify entrypoint script by removing redundant code
    Removed redundant lines for copying config and PID file cleanup.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,14 +8,4 @@ cp /app/config/config.json "${HOME}/.picoclaw/config.json"
 # Remove stale PID file from a previous container run.
 rm -f "${HOME}/.picoclaw/.picoclaw.pid"
 
-exec picoclaw gateway "$@"#!/bin/sh
-set -e
-
-# Copy config from app to picoclaw home
-mkdir -p "${HOME}/.picoclaw"
-cp /app/config/config.json "${HOME}/.picoclaw/config.json"
-
-# Remove stale PID file from a previous container run.
-rm -f "${HOME}/.picoclaw/.picoclaw.pid"
-
 exec picoclaw gateway "$@"

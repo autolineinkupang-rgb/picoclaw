b2415db7d9e75f8c815b5593b5507ee135cbfb1c
autolineinkupang-rgb <autolineinkupang@gmail.com>
2026-05-25 15:52:53 +0700

    Fix escape characters in sed commands

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,10 +6,9 @@ mkdir -p "${HOME}/.picoclaw"
 cp /app/config/config.json "${HOME}/.picoclaw/config.json"
 
 # Replace environment variables in config
-sed -i "s|\$TELEGRAM_BOT_TOKEN|${TELEGRAM_BOT_TOKEN}|g" "${HOME}/.picoclaw/config.json"
-sed -i "s|\$GROQ_API_KEY|${GROQ_API_KEY}|g" "${HOME}/.picoclaw/config.json"
-sed -i "s|\$TELEGRAM_USER_ID|${TELEGRAM_USER_ID}|g" "${HOME}/.picoclaw/config.json"
-
+sed -i "s|\\$TELEGRAM_BOT_TOKEN|${TELEGRAM_BOT_TOKEN}|g" "${HOME}/.picoclaw/config.json"
+sed -i "s|\\$GROQ_API_KEY|${GROQ_API_KEY}|g" "${HOME}/.picoclaw/config.json"
+sed -i "s|\\$TELEGRAM_USER_ID|${TELEGRAM_USER_ID}|g" "${HOME}/.picoclaw/config.json"
 # Remove stale PID file
 rm -f "${HOME}/.picoclaw/.picoclaw.pid"
 
